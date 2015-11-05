b7405d7e37b39a315f55c7372ceeec30e459f7c7
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-11-05 09:27:28 +0100

    Added publication of wsg-status to wsg-simulation.

--- iai_boxy_controller_configuration/scripts/wsg_50_controller.py
+++ iai_boxy_controller_configuration/scripts/wsg_50_controller.py
@@ -20,14 +20,16 @@
 
 import rospy
 import sys
-from iai_wsg_50_msgs.msg import SpeedCmd, PositionCmd
+from iai_wsg_50_msgs.msg import SpeedCmd, PositionCmd, Status
 from std_msgs.msg import Float32MultiArray, Header
 from iai_control_utils.jcontroller import JController
+from sensor_msgs.msg import JointState
 
 
 sub = None
 con = None
-
+js_sub = None
+status_pub = None
 
 def velocity_callback(msg):
   ''' Gets the velocity in mm/s and ignores the force '''
@@ -49,6 +51,13 @@ def velocity_callback(msg):
 
   con.des_pos_cb(cmd)
 
+
+def joint_state_callback(msg):
+  ''' Gets the current joint state and publishes a wsg-like status message. '''
+  status = Status()
+  status.width = msg.position[1] - msg.position[0]
+  status.speed = msg.velocity[1] - msg.velocity[0]
+  status_pub.publish(status)
 
 def position_callback(msg):
   ''' Get the position in mm with 110 meaning the fingers are at position -0.055 and 0.055 and the velocity in mm/s. The force is ignored. '''
@@ -79,6 +88,7 @@ if __name__ == '__main__':
 
   sub = rospy.Subscriber('~wsg_speed_in', SpeedCmd, velocity_callback)
   sub = rospy.Subscriber('~wsg_position_in', PositionCmd, position_callback)
-  
+  js_sub = rospy.Subscriber('~joint_states', JointState, joint_state_callback)
+  status_pub = rospy.Publisher('~status', Status, queue_size=3, tcp_nodelay=True, latch=False)
+
   rospy.spin()
-  
