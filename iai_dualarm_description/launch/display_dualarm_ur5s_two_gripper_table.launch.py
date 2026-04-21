c9138ad89124d110c68afd6bcfeb178929344f92
MideDay <l-hauschild@outlook.de>
2026-04-21 16:42:10 +0200

    adjusted urdf for xacro:ur_robot. Refactored launch files

--- iai_dualarm_description/launch/display_dualarm_ur5s_two_gripper_table.launch.py
+++ iai_dualarm_description/launch/display_dualarm_ur5s_two_gripper_table.launch.py
@@ -8,42 +8,48 @@ import os
 
 def generate_launch_description():
 
+    description_pkg = get_package_share_directory('iai_dualarm_description')
+
     robot_xacro_file = os.path.join(
-        get_package_share_directory("iai_dualarm_description"),
-        "robots",
-        "dualarm_ur5s_two_gripper_table.urdf.xacro",
+        description_pkg,
+        'robots',
+        'dualarm_ur5_two_gripper_table.urdf.xacro'
     )
 
     rviz_config_file = os.path.join(
-        get_package_share_directory("iai_dualarm_description"), "config", "urdf.rviz"
+        description_pkg,
+        'config',
+        'urdf.rviz'
     )
 
-    return LaunchDescription(
-        [
-            Node(
-                package="joint_state_publisher",
-                executable="joint_state_publisher",
-                name="joint_state_publisher",
-            ),
-            Node(
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                name="robot_state_publisher",
-                output="screen",
-                parameters=[
-                    {
-                        "robot_description": ParameterValue(
-                            Command(["xacro ", robot_xacro_file]), value_type=str
-                        )
-                    }
-                ],
-            ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                output="screen",
-                arguments=["-d", rviz_config_file],
-            ),
-        ]
+    robot_description = ParameterValue(
+        Command(['xacro ', robot_xacro_file]),
+        value_type=str
     )
+
+    return LaunchDescription([
+
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            parameters=[{'robot_description': robot_description}]
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file]
+        )
+    ])
--- iai_dualarm_description/launch/upload_dualarm_ur5s_two_gripper_table.launch.py
+++ iai_dualarm_description/launch/upload_dualarm_ur5s_two_gripper_table.launch.py
@@ -5,52 +5,41 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-
 def generate_launch_description():
 
-    urdf_name = LaunchConfiguration("urdf_name")
-    param_name = LaunchConfiguration("param_name")
-    transmission_hw_interface = LaunchConfiguration("transmission_hw_interface")
+    urdf_name = LaunchConfiguration('urdf_name')
+    param_name = LaunchConfiguration('param_name')
+    transmission_hw_interface = LaunchConfiguration('transmission_hw_interface')
 
     # Default paths
     urdf_file = os.path.join(
-        get_package_share_directory("iai_dualarm_description"),
-        "robots",
-        "dualarm_ur5s_two_gripper_table.urdf.xacro",
+        get_package_share_directory('iai_dualarm_description'),
+        'robots',
+        'dualarm_ur5s_one_gripper_table.urdf.xacro'
     )
     kinematics_file = os.path.join(
-        get_package_share_directory("iai_ur_description"), "config", "ur5_default.yaml"
+        get_package_share_directory('iai_ur_description'),
+        'config',
+        'ur5_default.yaml'
     )
 
-    robot_description = Command(
-        [
-            "xacro ",
-            urdf_file,
-            " transmission_hw_interface:=",
-            "hardware_interface/PositionJointInterface",
-            " kinematics_config_left:=",
-            kinematics_file,
-            " kinematics_config_right:=",
-            kinematics_file,
-        ]
-    )
+    robot_description = Command([
+        'xacro ', urdf_file,
+        ' transmission_hw_interface:=', 'hardware_interface/PositionJointInterface',
+        ' kinematics_config_left:=', kinematics_file,
+        ' kinematics_config_right:=', kinematics_file
+    ])
 
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "urdf_name", default_value="dualarm_ur5s_two_gripper.urdf.xacro"
-            ),
-            DeclareLaunchArgument("param_name", default_value="robot_description"),
-            DeclareLaunchArgument(
-                "transmission_hw_interface",
-                default_value="hardware_interface/PositionJointInterface",
-            ),
-            Node(
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                name="robot_state_publisher",
-                output="screen",
-                parameters=[{"robot_description": robot_description}],
-            ),
-        ]
-    )
+    return LaunchDescription([
+        DeclareLaunchArgument('urdf_name', default_value='dualarm_ur5_two_gripper_table.urdf.xacro'),
+        DeclareLaunchArgument('param_name', default_value='robot_description'),
+        DeclareLaunchArgument('transmission_hw_interface', default_value='hardware_interface/PositionJointInterface'),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        )
+    ])
