6dc7e616dcc16443426ba3deea5ca2baa9a10a12
Mitesh Savsaviya <135125057+mitsav01@users.noreply.github.com>
2026-05-13 13:35:19 +0200

    Add joint_state_publisher_gui node to launch file

--- iai_daisy_description/launch/display.launch.py
+++ iai_daisy_description/launch/display.launch.py
@@ -36,7 +36,12 @@ def generate_launch_description():
             output='screen',
             parameters=[{'robot_description': robot_description}]
         ),
-
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            parameters=[{'robot_description': robot_description}]
+        ),
         Node(
             package='rviz2',
             executable='rviz2',
